e031d46a4b3d12f86158997e8876a240966e333b
Yury Prudnikov <struck.yury@gmail.com>
2021-07-05 11:42:38 +0300

    Add function transform JsonObject to CoinPriceInfo

--- app/src/main/java/com/example/cryptoapp/CoinViewModel.kt
+++ app/src/main/java/com/example/cryptoapp/CoinViewModel.kt
@@ -34,6 +34,27 @@ class CoinViewModel(application: Application) : AndroidViewModel(application) {
             })
     }
 
+    /*  This function transform JsonObject "CoinPriceInfoRawData" to the "List<CoinPriceInfo>",
+        this will allow to get a multiconversion into different currencies
+     */
+    private fun getPriceListFromRawData(coinPriceInfoRawData: CoinPriceInfoRawData): List<CoinPriceInfo> {
+        val result = ArrayList<CoinPriceInfo>()
+        val jsonObject = coinPriceInfoRawData.coinPriceInfoJsonObject
+        if (jsonObject == null) return result
+        val coinKeySet = jsonObject.keySet()
+        for (coinKey in coinKeySet) {
+            val currencyJson = jsonObject.getAsJsonObject(coinKey)
+            val currencyKeySet = currencyJson.keySet()
+            for (currencyKey in currencyKeySet) {
+                val priceInfo = Gson().fromJson(
+                    currencyJson.getAsJsonObject(currencyKey),
+                    CoinPriceInfo::class.java
+                )
+                result.add(priceInfo)
+            }
+        }
+        return result
+    }
 
     override fun onCleared() {
         super.onCleared()
