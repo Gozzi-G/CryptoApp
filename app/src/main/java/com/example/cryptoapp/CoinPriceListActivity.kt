bc8a956d9651653e216dd2373bd5180428f4c38a
Yury Prudnikov <struck.yury@gmail.com>
2021-07-07 22:39:05 +0300

    Change color sheme

--- app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
@@ -3,28 +3,24 @@ package com.example.cryptoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.example.cryptoapp.api.ApiFactory
-import io.reactivex.android.schedulers.AndroidSchedulers
+import com.example.cryptoapp.adapter.CoinInfoAdapter
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_coin_price_list.*
 
 
-class MainActivity : AppCompatActivity() {
-    private val compositeDisposable = CompositeDisposable()
+class CoinPriceListActivity : AppCompatActivity() {
     private lateinit var viewModel : CoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_coin_price_list)
+        val adapter = CoinInfoAdapter()
+        rvCoinPriceList.adapter = adapter
         viewModel = ViewModelProviders.of(this)[CoinViewModel::class.java]
-//        viewModel.priceList.observe(this, Observer {
-//            Log.d("test", "Success in activity: $it")
-//        })
-        viewModel.getDetailInfo("BTC").observe(this, Observer {
-            Log.d("test", "Success in activity: $it")
+        viewModel.priceList.observe(this, Observer {
+            adapter.coinInfoList = it
         })
     }
 }
