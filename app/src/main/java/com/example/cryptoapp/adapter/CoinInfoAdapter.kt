7d9efe6de9f10badc5115dd26576bc412419c915
Yury Prudnikov <struck.yury@gmail.com>
2021-07-07 22:42:25 +0300

    Add strin resources

--- app/src/main/java/com/example/cryptoapp/adapter/CoinInfoAdapter.kt
+++ app/src/main/java/com/example/cryptoapp/adapter/CoinInfoAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.cryptoapp.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +11,7 @@ import com.example.cryptoapp.pojo.CoinPriceInfo
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_coin_info.view.*
 
-class CoinInfoAdapter: RecyclerView.Adapter<CoinInfoAdapter.CoinInfoViewHolder>() {
+class CoinInfoAdapter(private val context: Context): RecyclerView.Adapter<CoinInfoAdapter.CoinInfoViewHolder>() {
 
     var coinInfoList: List<CoinPriceInfo> = listOf()
     set(value) {
@@ -27,10 +28,12 @@ class CoinInfoAdapter: RecyclerView.Adapter<CoinInfoAdapter.CoinInfoViewHolder>(
 
 
     override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
+        val symbolsTemplate = context.resources.getString(R.string.symbols_template)
+        val lastUpdateSymbols = context.resources.getString(R.string.last_update_template)
         val coin = coinInfoList[position]
-        holder.tvSymbols.text = coin.fromSymbol + "/" + coin.toSymbol
+        holder.tvSymbols.text = String.format(symbolsTemplate, coin.fromSymbol, coin.toSymbol)
         holder.tvPrice.text = coin.price
-        holder.tvUpDate.text = coin.getFormattedTime()
+        holder.tvUpDate.text = String.format(lastUpdateSymbols, coin.getFormattedTime())
         Picasso.get().load(coin.getFullImageUrl()).into(holder.ivLogoCoin)
     }
 
