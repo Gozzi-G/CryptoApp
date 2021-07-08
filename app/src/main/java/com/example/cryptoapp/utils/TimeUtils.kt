1f4ec2f1f1a214e9509d5f5610cb7724829a18fc
Yury Prudnikov <struck.yury@gmail.com>
2021-07-08 10:37:08 +0300

    Change timestamp converter

--- app/src/main/java/com/example/cryptoapp/utils/TimeUtils.kt
+++ app/src/main/java/com/example/cryptoapp/utils/TimeUtils.kt
@@ -9,7 +9,7 @@ fun convertTimestampToTime(timestamp: Long?): String {
     if (timestamp == null) return ""
     val stamp = Timestamp(timestamp * 1000)
     val date = Date(stamp.time)
-    val pattern = "HH:mm:ss"
+    val pattern = "HH:mm"
     val sdf = SimpleDateFormat(pattern, Locale.getDefault())
     sdf.timeZone = TimeZone.getDefault()
     return sdf.format(date)
