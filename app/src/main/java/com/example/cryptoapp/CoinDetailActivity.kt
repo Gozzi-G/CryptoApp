784352d8d7a76d648cdbf21e767005cb72f98112
Yury Prudnikov <struck.yury@gmail.com>
2021-07-08 10:45:26 +0300

    Create CoinDetailActivity

---
+++ app/src/main/java/com/example/cryptoapp/CoinDetailActivity.kt
@@ -0,0 +1,17 @@
+package com.example.cryptoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+
+class CoinDetailActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coin_detail)
+
+    }
+}
